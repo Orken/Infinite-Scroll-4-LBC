3a44c8ecc55140a24b95593215ce0300dd08c412
Orken <subotm@free.fr>
2014-03-21 22:21:28 +0100

    Update Infinite.js
    - Prise en charge du fait qu'il n'y a plus de page à charger
    - Numérotation des pages

--- Infinite.js
+++ Infinite.js
@@ -6,35 +6,60 @@
 //
 // ==/UserScript==
 this.$ = this.jQuery = jQuery.noConflict(true);
-var load = false;
-var offset = $('#paging') .offset() .top;
+
+var load     = false;
+var offset   = $('#paging') .offset() .top;
+var $listlbc = $('.list-lbc');
+var $content = $('.content-border');
+
 var ajoutePage = function (html) {
-    var $lastPage = $('.list-lbc a:last');
-    var $block = $('.content-border');
+
     $('#paging') .remove();
-    var $html = $(html);
-    var $ads = $html.find('.list-lbc a');
-    var $page = $html.find('#paging');
-    $lastPage.after($ads);
-    $block.after($page);
+
+    var $html    = $(html);
+    var $ads     = $html.find('.list-lbc a');
+    var $page    = $html.find('#paging');
+
+    $listlbc.append($ads);
+    $content.after($page);
+
     offset = $('#paging') .offset() .top;
-    load = false;
+    load   = false;
 };
-$(window) .scroll(function () {
-    var height = window.innerHeight;
-    var scrollY = window.scrollY;
+
+$(window).scroll(function () {
+    var height   = window.innerHeight;
+    var scrollY  = window.scrollY;
     if ((!load) && (scrollY > (offset - height))) {
-        load = true;
-        var $url = $('li.page a:last') .get(0);
+        load            = true;
         var $pagination = $('#paging');
-        $pagination.empty().html('<center>Chargement...</center>');
-        $.ajax({
-            url: $url.href,
-            success: ajoutePage,
-            error: function (error) {
-                alert('ko');
-                console.log(error);
-            },
-        });
+        var $url        = $('li.page:last a');
+        if ($url.length>0) { 
+            var selected = $pagination.find('.selected').next().text();
+            var url      = $url.get(0).href;
+
+            var $a = $('<a/>',{href:url})
+                .append( 
+                    $('<div/>',{class:'lbc'})
+                        .append( $('<div/>',{class:'date'}) )
+                        .append( $('<div/>',{class:'image'}) )
+                        .append( $('<div/>',{class:'detail'}).text('PAGE '+selected) )
+                        .append( $('<div/>',{class:'clear'}) )
+                );
+
+            $listlbc.append($a);            
+            $pagination.empty().html('<center>Chargement...</center>');
+            $.ajax({
+                url: url,
+                success: ajoutePage,
+                error: function (error) {
+                    alert('ko');
+                    console.log(error);
+                },
+            });
+        } else {
+            $pagination.empty().html('<center>Plus de page.</center>');
+        }
     }
+
 });
